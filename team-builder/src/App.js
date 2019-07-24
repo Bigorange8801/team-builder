a8742b7039345075f3c21816b193057f965ba79e
John <cjd8801@gmail.com>
2019-07-24 17:57:56 -0500

    cards added

---
+++ team-builder/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  const [team,teamMember] = useState({})
+
+
+
+
+
+
+  return (
+    <div className="App">
+     <Form/>
+    </div>
+  );
+}
+
+export default App;
