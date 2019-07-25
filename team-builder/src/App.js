699fc752c3ba7cc2c0c2bde408b16a78932db96b
John <cjd8801@gmail.com>
2019-07-25 09:56:08 -0500

    updates

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Member from "./components/Member";
+import Form from "./components/Form";
 
 function App() {
-  const [team,teamMember] = useState({})
+  const [members, setMembers] = useState([]);
 
+  const submitMember = member => setMembers([...members, member]);
+  const update = uMember =>
+    setMembers([
+      ...members.map(member => {
+        if (member.id === uMember.id) {
+          return uMember;
+        }
+        return member;
+      })
+    ]);
 
-
-
-
+  console.log("members", members);
 
   return (
     <div className="App">
-     <Form/>
+      {members.map((member, i) => (
+        <Member member={member} key={i} update={update} />
+      ))}
+      <Form add={submitMember} />
     </div>
   );
 }
